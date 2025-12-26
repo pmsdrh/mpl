d943f3946c068e1143024c2de4969711cb278a60
pmsdrh <mohammadsalehramandi@gmail.com>
2025-12-26 20:28:32 +0330

    feat(musics): add Music Model

--- musics/models.py
+++ musics/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 class PlayList(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=False, default="-")
     description = models.TextField()
-    slug = models.SlugField(mex_length=50, null=False, default="")
+    slug = models.SlugField(max_length=50, null=False, default="")
+
+class Music(models.Model):
+    name = models.CharField(max_length=200)
+    caption = models.TextField()
+    link = models.TextField()
+
