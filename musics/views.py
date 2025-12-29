3cdd05af79768b2d3a9f0625ff196dd68af5185c
pmsdrh <mohammadsalehramandi@gmail.com>
2025-12-29 14:55:04 +0330

    feat(musics): add PlayList viewset

--- musics/views.py
+++ musics/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serilizers import PlayListSerilizer
+from .models import PlayList
+from rest_framework.response import Response as R
 
 # Create your views here.
+
+
+class PlayListViewSet(viewsets.ViewSet):
+    def list(self, request):
+        srz_data = PlayListSerilizer(instance=PlayList.objects.all(), many=True)
+        return R(data=srz_data.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    
